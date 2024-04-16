30fae7f90d57a91e1c1565d9b2475629c021e28b
krisHOI4Emporium <90762669+krisHOI4Emporium@users.noreply.github.com>
2024-04-16 08:56:48 +0200

    chuj, rozdzielę te efekty zmiany nazw VP do osobncyh plików

--- python/generate_vp_rename_code.py
+++ python/generate_vp_rename_code.py
@@ -13,6 +13,16 @@ kashubian = open("change_vp_name_to_kashubian.txt","w",newline='\r\n')
 notthatprussian = open("change_vp_name_to_notthatprussian.txt","w",newline='\r\n')
 silesian = open("change_vp_name_to_silesian.txt","w",newline='\r\n')
 polish = open("reset_vp_name.txt","w",newline='\r\n')
+czepanese.write("change_vp_name_to_czepanese = {\n")
+kashubian.write("change_vp_name_to_kashubian = {\n")
+german.write("change_vp_name_to_german = {\n")
+nordic.write("change_vp_name_to_nordic = {\n")
+latin.write("change_vp_name_to_latin = {\n")
+lechite.write("change_vp_name_to_lechite = {\n")
+russian.write("change_vp_name_to_russian = {\n")
+kolonkish.write("change_vp_name_to_kolonkish = {\n")
+notthatprussian.write("change_vp_name_to_notthatprussian = {\n")
+polish.write("reset_vp_name = {\n")
 
 while True:
     line = f.readline();
@@ -25,36 +35,47 @@ while True:
         else: language = "POL";
         #print(language);
         if language == "POL":
-            polish.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"\n\t}\n}\n");
+            polish.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"\n\t\t}\n\t}\n");
         elif language == "CAL":
-            latin.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_CAL\n\t}\n}\n");
+            latin.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_CAL\n\t\t}\n\t}\n");
         elif language == "CZE":
-            czepanese.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_CZE\n\t}\n}\n");
+            czepanese.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_CZE\n\t\t}\n\t}\n");
         elif language == "GER":   
-            german.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_GER\n\t}\n}\n");
+            german.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_GER\n\t\t}\n\t}\n");
         elif language == "JOM":
-            nordic.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_JOM\n\t}\n}\n");
+            nordic.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_JOM\n\t\t}\n\t}\n");
         elif language == "LEH":
-            lechite.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_LEH\n\t}\n}\n");
+            lechite.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_LEH\n\t\t}\n\t}\n");
         elif language == "UKROS":
-            russian.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_UKROS\n\t}\n}\n");
+            russian.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_UKROS\n\t\t}\n\t}\n");
         elif language == "USP":
-            kolonkish.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_USP\n\t}\n}\n");
+            kolonkish.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_USP\n\t\t}\n\t}\n");
         elif language == "KSZ":
-            kashubian.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_KSZ\n\t}\n}\n");
+            kashubian.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_KSZ\n\t\t}\n\t}\n");
         elif language == "PRU":
-            notthatprussian.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_PRU\n\t}\n}\n");
+            notthatprussian.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_PRU\n\t\t}\n\t}\n");
         elif language == "SIL":
-            silesian.write("if = {\n\tlimit = {\n\t\tcontrols_province = "+province_id+"\n\t}\n\tset_province_name = {\n\t\tid = "+province_id+"\n\t\tname = VICTORY_POINTS_"+province_id+"_SIL\n\t}\n}\n");
+            silesian.write("\tif = {\n\t\tlimit = {\n\t\t\tcontrols_province = "+province_id+"\n\t\t}\n\t\tset_province_name = {\n\t\t\tid = "+province_id+"\n\t\t\tname = VICTORY_POINTS_"+province_id+"_SIL\n\t\t}\n\t}\n");
 f.close();
+polish.write("}")
 polish.close();
+latin.write("}")
 latin.close();
+czepanese.write("}")
 czepanese.close();
+german.write("}")
 german.close();
+nordic.write("}")
 nordic.close();
+lechite.write("}")
 lechite.close();
+russian.write("}")
 russian.close();
+kolonkish.write("}")
 kolonkish.close();
-kashubian.close();
+kashubian.write("}")
+kashubian.close()
+notthatprussian.write("}");
 notthatprussian.close();
+silesian.write("}")
 silesian.close();
