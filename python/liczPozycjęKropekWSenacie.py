c2107afd6c2531b299b7669a4830ba29534a006a
krisHOI4Emporium <90762669+krisHOI4Emporium@users.noreply.github.com>
2024-05-21 20:25:29 +0200

    better senate

--- python/liczPozycjęKropekWSenacie.py
+++ python/liczPozycjęKropekWSenacie.py
@@ -34,117 +34,274 @@ while i < 900:
     if i < 21:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row1 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row1 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row1 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row1 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row1 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row1 < "+str(j+1)+" }\n\t\t\t"
+                "check_variable = { CAL_optimates_hard_at_row1 > "+str(20-j)+" }\n\t\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_optimates_hard_seat\n\t}\n\t")
     if i >= 21 and i < 45:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row2 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row2 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row2 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row2 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row2 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row2 < "+str(j+1)+" }\n\t\t\t"
+                "check_variable = { CAL_optimates_hard_at_row2 > "+str(23-j)+" }\n\t\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_optimates_hard_seat\n\t}\n\t")
     if i >= 45 and i < 71:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row3 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row3 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row3 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row3 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row3 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row3 < "+str(j+1)+" }\n\t\t\t"
+                "check_variable = { CAL_optimates_hard_at_row3 > "+str(25-j)+" }\n\t\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_optimates_hard_seat\n\t}\n\t")
     if i >= 71 and i < 100:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row4 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row4 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row4 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row4 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row4 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row3 < "+str(j+1)+" }\n\t\t\t"
+                "check_variable = { CAL_optimates_hard_at_row3 > "+str(28-j)+" }\n\t\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_optimates_hard_seat\n\t}\n\t")
     if i >= 100 and  i < 125:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row5 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row5 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row5 < "+str(j+1)+"}\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row5 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row5 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 125 and  i < 150:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row6 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row6 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row6 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row6 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row6 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 150 and  i < 183:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row7 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row7 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row7 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row7 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row7 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 183 and  i < 218:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row8 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row8 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row8 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row8 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row8 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 218 and  i < 257:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row9 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row9 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row9 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row9 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row9 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 257 and  i < 300:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row10 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row10 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row10 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row10 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row10 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 300 and  i < 347:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row11 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row11 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row11 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row11 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row11 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 347 and  i < 456:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row12 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row12 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row12 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row12 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row12 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 399 and  i < 456:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row13 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row13 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row13 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row13 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row13 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 456 and  i < 516:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row14 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row14 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row14 < "+str(j+1)+"}\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row14 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row14 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 516 and  i < 579:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row15 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row15 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row15 < "+str(j+1)+"}\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row15 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row15 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 579 and  i < 648:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row16 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row16 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row16 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row16 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row16 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 648 and  i < 724:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row17 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row17 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row17 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row17 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row17 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 724 and  i < 808:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row18 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row18 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row18 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row18 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row18 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     if i >= 808:
         scriptedLocs.write("text = {\n\t\t"+
             "trigger = {\n\t\t\t"+
-                "check_variable = { CAL_populares_at_row19 > "+str(j)+" }\n\t\t"
+                "check_variable = { CAL_populares_at_row19 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row19 < "+str(j+1)+" }\n\t\t"
             "}\n\t\t"+
             "localization_key = GFX_populares_seat\n\t}\n\t")
+        scriptedLocs.write("text = {\n\t\t"+
+            "trigger = {\n\t\t\t"+
+                "check_variable = { CAL_populares_at_row19 > "+str(j)+" }\n\t\t\t"
+                "check_variable = { CAL_populares_hard_at_row19 > "+str(j)+" }\n\t\t"
+            "}\n\t\t"+
+            "localization_key = GFX_populares_hard_seat\n\t}\n\t")
     scriptedLocs.write("text = {\n\t\t"+
             "localization_key = GFX_optimates_seat\n\t}\n")
     scriptedLocs.write("}\n")
